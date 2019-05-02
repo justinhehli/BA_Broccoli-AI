5713b7d976de37d6447025f419db0ce4043be151
Justin Hehli <juhe95@hotmail.com>
2019-05-02 15:26:04 +0200

    Installation of GDAL and rasterio: whl-files and Script to fix InstallationPackages.txt

---
+++ InstallationPackages_UpdateWheels.py
@@ -0,0 +1,6 @@
+s = open("InstallationPackages.txt").read()
+s = s.replace('rasterio==1.0.22', ".\\_whl\\rasterio-1.0.22-cp37-cp37m-win_amd64.whl")
+s = s.replace('GDAL==2.4.1', ".\\_whl\\GDAL-2.4.1-cp37-cp37m-win_amd64.whl")
+f = open("InstallationPackages.txt", 'w')
+f.write(s)
+f.close()
